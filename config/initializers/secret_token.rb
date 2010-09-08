dcdc0b31448dc7906ef88a63f1e1476b3179bcff
Toshiaki Katayama <k@bioruby.org>
2010-09-08 18:12:10 +0900

    TogoDB+Rails3+Ruby1.9.2 test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Togodb3::Application.config.secret_token = '30d18f677fa5a8f6dc7aa5605f99165f13fb737b156cf7f38f0ca9bc46ace2044fe3cbaa2363e59706f1d4c7681f377321eb88bde5ae8c8d006097e6f4431686'
